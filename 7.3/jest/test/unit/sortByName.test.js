66e0f405cd8a6f2ee2eaef0d1f21c3f51d8f8846
Vladimir Pavlyukov <yahusim@mail.ru>
2023-03-22 01:25:36 +0300

    add gitignore, setting up jest, completed 7.3.1 task

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,26 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
+      "Властелин Колец",
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const resalt = sorting.sortByName(input);
+
+    expect(resalt).toEqual(expected);
+  });
+
+  test("Function should throw exception when user without args", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError);
   });
 });
