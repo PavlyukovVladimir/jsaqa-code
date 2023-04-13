99b22417bf5497aaf2e71e4bb4110d8cf3b9fb99
Vladimir <vladimir.pavlyukov@thrive.io>
2023-04-13 17:31:31 +0300

    end refactoring both tasks

--- 7.5/home-works/features/search.feature
+++ 7.5/home-works/features/search.feature
@@ -1,6 +1,20 @@
 Feature: Booking tickets
 
-    Scenario: booking tickets after the current day
+    Scenario: Current day ticket booking
+        Given user is on main page of tmweb
+        When user randomly chooses one of available seance
+        Then user will see the ticket booking page
+        When user randomly chooses one of available chair
+        Then user sees the page for checking booked tickets
+        When user will confirm the booking
+        Then user received a QR code
+
+    Scenario: Not posible ticket booking for seances that have been started
+        Given user is on main page of tmweb
+        When user randomly selects a session that starts earlier than the current moment
+        Then user will remain on the current day's movie poster page
+
+    Scenario: One of next days ticket booking
         Given user is on main page of tmweb
         When user randomly chooses one of 6 days starting from the next day
         Then user sees the poster of the selected day
@@ -10,8 +24,3 @@ Feature: Booking tickets
         Then user sees the page for checking booked tickets
         When user will confirm the booking
         Then user received a QR code
-
-    Scenario: booking tickets after the current day
-        Given user is on main page of tmweb
-        When user randomly chooses one of unavailable seance
-        Then user will remain on the current day's movie poster page
--- 7.5/home-works/features/step_definitions/search.steps.js
+++ 7.5/home-works/features/step_definitions/search.steps.js
@@ -1,8 +1,11 @@
 // #region require
 
 const puppeteer = require("puppeteer");
-const chai = require("chai");
-const expect = chai.expect;
+
+const { expect } = require("chai");
+
+var HomePage = require("../../lib/pages/home.page");
+
 const {
   Given,
   When,
@@ -11,27 +14,22 @@ const {
   After,
   setDefaultTimeout,
 } = require("@cucumber/cucumber");
-const {
-  getText,
-  getFreeChairs,
-  getSeances,
-  getChairs,
-} = require("../../lib/commands.js");
-const { getCurrentDayTimestamp } = require("../../lib/util.js");
-setDefaultTimeout(30000);
+
 // #endregion require
 // #region Before/after
+setDefaultTimeout(30000);
 
 Before(async function () {
   const browser = await puppeteer.launch({
     headless: false,
     slowMo: 10,
-    timeout: 100000,
+    timeout: 30000,
   });
   this.browser = browser;
   const page = await browser.newPage();
   this.page = page;
-  this.currentDayTimestamp = getCurrentDayTimestamp();
+  this.homePage = new HomePage(page, "http://qamid.tmweb.ru/client/index.php");
+  await this.homePage.goToPage();
 });
 
 After(async function () {
@@ -45,9 +43,7 @@ After(async function () {
 
 // Переход на страницу афиши
 Given("user is on main page of tmweb", async function () {
-  return await this.page.goto(`http://qamid.tmweb.ru/client/index.php`, {
-    setTimeout: 10000,
-  });
+  return await this.homePage.checkPage();
 });
 
 // #endregion Given
@@ -57,84 +53,37 @@ Given("user is on main page of tmweb", async function () {
 When(
   "user randomly chooses one of {int} days starting from the next day",
   async function (int) {
-    let shiftDays = Math.floor(Math.random() * int) + 1;
-    this.otherDayTimestamp =
-      this.currentDayTimestamp + shiftDays * 24 * 60 * 60;
-
-    let selector = `.page-nav a[data-time-stamp="${this.otherDayTimestamp}"]`;
-    (await this.page.$(selector)).click();
+    await this.homePage.randomlySelectWeekDay({ min: 2, max: int });
   }
 );
 
 // Когда пользователь случайным образом выбирает доступный сеанс
 When("user randomly chooses one of available seance", async function () {
-  const page = await this.page;
-  const seances = await getSeances(page);
-  const currentMomentTimestsmp = Math.floor(2 + Date.now() / 1000);
-  const availableSeances = await seances.filter(
-    (seance) => seance.ts > currentMomentTimestsmp
-  );
-  const length = await availableSeances.length;
-  // кликнуть случайный доступный сеанс
-  let timestamp = await availableSeances[Math.floor(Math.random() * length)].ts;
-
-  (
-    await page.$(`a.movie-seances__time[data-seance-time-stamp="${timestamp}"]`)
-  ).click();
-  await page.waitForSelector(".buying-scheme__row");
+  this.chairsChoicePage = await this.homePage.randomlySelectSeance(true);
 });
 
 // Когда пользователь случайным образом выбирает недоступный сеанс
-When("user randomly chooses one of unavailable seance", async function () {
-  const page = await this.page;
-  const seances = await getSeances(page);
-  const currentMomentTimestsmp = Math.floor(Date.now() / 1000);
-  const availableSeances = await seances.filter(
-    (seance) => seance.ts < currentMomentTimestsmp
-  );
-  const length = await availableSeances.length;
-  // кликнуть случайный доступный сеанс
-  this.seanceTimestamp = await availableSeances[
-    Math.floor(Math.random() * length)
-  ].ts;
-  (
-    await page.$(
-      `a.movie-seances__time[data-seance-time-stamp="${await this
-        .seanceTimestamp}"]`
-    )
-  ).click();
-});
+When(
+  "user randomly selects a session that starts earlier than the current moment",
+  async function () {
+    const date = new Date();
+    const timeNowStr = `${date.getHours()}:${date.getMinutes() + 1}`;
+    this.chairsChoicePage = await this.homePage.randomlySelectSeanceBeforeTime(
+      timeNowStr
+    );
+  }
+);
 
 // Когда пользователь случайным образом выбирает кресло
 When("user randomly chooses one of available chair", async function () {
-  const page = await this.page;
-
-  // получить места
-  let chairs = await getChairs(page);
-  // выбрать любое доступное к заказу
-  let chair = await getFreeChairs(chairs, 1, "any")[0];
-  await page.click(
-    `.buying-scheme__row:nth-child(${await (chair.row +
-      1)}) .buying-scheme__chair:nth-child(${await (chair.chair + 1)})`
-  );
-  // подождать пока место станет отмеченным
-  await page.waitForSelector(
-    `.buying-scheme__row:nth-child(${
-      chair.row + 1
-    }) .buying-scheme__chair:nth-child(${
-      chair.chair + 1
-    }).buying-scheme__chair_selected`
-  );
-  // Кликнуть кнопку "забронировать"
-  await page.click(".acceptin-button");
+  const chair = await this.chairsChoicePage.getRandomFreeChair();
+  await this.chairsChoicePage.selectChairs(chair);
+  this.viewBookingPage = await this.chairsChoicePage.pressBookButton();
 });
 
 // Когда пользователь подтверждает бронирование
 When("user will confirm the booking", async function () {
-  const page = await this.page;
-
-  // кликнуть кнопку бронирования
-  (await page.$(".acceptin-button")).click();
+  this.qrPage = await this.viewBookingPage.pressBookButton();
 });
 
 // #endregion When
@@ -142,59 +91,37 @@ When("user will confirm the booking", async function () {
 
 // Тогда пользователь видит афишу выбранного дня
 Then("user sees the poster of the selected day", async function () {
-  await this.page.waitForSelector(
-    `.page-nav a.page-nav__day_chosen[data-time-stamp="${this.otherDayTimestamp}"]`
-  );
+  await this.homePage.checkSelectedWeekDay();
 });
 
 // Тогда пользователь видит страницу бронирования билетов
 Then("user will see the ticket booking page", async function () {
-  await this.page.waitForSelector(".buying-scheme__row");
-
-  const actual1 = await getText(this.page, ".buying__info-start");
-  expect(actual1).contain("Начало сеанса");
+  await this.chairsChoicePage.checkPage();
 });
 
 // Тогда пользователь видит страницу проверки забронированных билетов
 Then("user sees the page for checking booked tickets", async function () {
-  const page = await this.page;
-
-  // дождаться появления заголовка блока проверки билетов
-  await page.waitForSelector("h2.ticket__check-title");
-
-  const actual2 = await getText(page, "h2.ticket__check-title");
-  expect(actual2).contain("Вы выбрали билеты");
+  await this.viewBookingPage.checkPage();
 });
 
 // Тогда пользователь получит QR код
 Then("user received a QR code", async function () {
-  const page = await this.page;
-
-  // дождаться появления QR картинки
-  await page.waitForSelector("img.ticket__info-qr");
-
-  const actual3 = await getText(page, "h2.ticket__check-title");
-  expect(actual3).contain("Электронный билет");
+  await this.qrPage.checkPage();
 });
 
 // Тогда пользователь останется на странице афиши текущего дня
 Then(
   "user will remain on the current day's movie poster page",
   async function () {
-    const page = await this.page;
+    let errMsg;
+    const time = 5000;
+    try {
+      await this.chairsChoicePage.checkPage(time);
+    } catch (e) {
+      errMsg = e.message;
+    }
 
-    await page.waitForTimeout(2000);
-
-    await page.waitForSelector(
-      `a.acceptin-button-disabled.movie-seances__time[data-seance-time-stamp="${await this
-        .seanceTimestamp}"]`
-    );
-
-    // клик по сеансам до настоящего времени никуда не переместит
-    // проверка состоит в удачном ожидании элемента присущего этой же странице, спустя таймаут
-    await page.waitForSelector("a.page-nav__day_today.page-nav__day_chosen", {
-      timeout: 1000,
-    });
+    expect(errMsg).to.contain(`Waiting failed: ${time}ms exceeded`);
   }
 );
 
